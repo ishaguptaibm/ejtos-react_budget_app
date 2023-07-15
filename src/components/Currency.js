bda3dc77baa2326ae458b1f531f192c4eae0761d
ishaguptaibm <102983973+ishaguptaibm@users.noreply.github.com>
2023-07-15 16:20:34 +0530

    Update Currency.js

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,12 +1,12 @@
-import { Dropdown } from 'bootstrap';
 import React, { useContext,useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Currency = () => {
-    const [name, setName] = useState('');
+    const { expenses,budget1 } = useContext(AppContext);
+    const [currency, setNameCurrency] = useState('');
     return (
         <div >
             <label style={{ backgroundColor: 'green', color: 'white'}} htmlFor="dropdown">Currency : (
-<select id="dropdown" style={{ backgroundColor: 'green', color: 'white',hover:'grey' }} value={name} onChange={(event) => setName(event.target.value)}>
+<select id="dropdown" style={{ backgroundColor: 'green', color: 'white',hover:'grey' }} value={currency} onChange={(event) => setNameCurrency(event.target.value)}>
                         
                         <option value="$ Dollar" name="$ Dollar"> $ Dollar</option>
                 <option value="£ Pound" name="£ Pound">£ Pound</option>
@@ -20,4 +20,3 @@ const Currency = () => {
     );
 };
 export default Currency;
-
